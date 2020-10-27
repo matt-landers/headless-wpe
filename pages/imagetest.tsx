5b6de441c65777a21bc8fd986e66916b78504867
Matt Landers <matt@platypi.io>
2020-10-27 13:07:41 -0500

    fix image page

--- pages/imagetest.tsx
+++ pages/imagetest.tsx
@@ -4,8 +4,8 @@ import { useRouter } from "next/router";
 const ImageTest = () => {
   const router = useRouter();
 
-  const width: number = parseInt(router?.query?.width as string) || 480;
-  const height: number = parseInt(router?.query?.height as string) || 320;
+  const width: number = parseInt(router?.query?.width as string) || 1024;
+  const height: number = parseInt(router?.query?.height as string) || 576;
 
   return (
     <Image
